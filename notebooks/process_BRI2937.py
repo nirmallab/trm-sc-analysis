cd01658bc4ffcfbc2baba876286ad4c228e58f03
Tyler Jost <tjost@bwh.harvard.edu>
2026-01-15 16:24:20 -0500

    fix: readd celltypist

--- notebooks/process_BRI2937.py
+++ notebooks/process_BRI2937.py
@@ -193,5 +193,52 @@ sc.pl.umap(
     legend_loc="on data",
 )
 # %%
+import celltypist
+from celltypist import models
+immuneHigh = models.Model.load(model="../models/Immune_All_High_Mouse.pkl")
+immuneLow = models.Model.load(model="../models/Immune_All_Low_Mouse.pkl")
+
+adata_celltypist = adata.copy()  
+adata_celltypist.X = adata.layers["log1p_norm"]  
+# normalize to 10,000 counts per cell (celltypist wants this)
+sc.pp.normalize_total(
+    adata_celltypist, target_sum=10**4
+)  
+sc.pp.log1p(adata_celltypist)
+predictions_high = celltypist.annotate(
+    adata_celltypist, model=immuneHigh, majority_voting=True
+)
+predictions_low = celltypist.annotate(
+    adata_celltypist, model=immuneLow, majority_voting=True
+)
+predictions_high_adata = predictions_high.to_adata().copy()
+predictions_low_adata = predictions_low.to_adata().copy()
+
+sc.pl.umap(
+    predictions_high_adata,
+    color=["majority_voting", "conf_score"],
+    frameon=False,
+    sort_order=False,
+    wspace=0.3,
+)
+sc.pl.umap(
+    predictions_low_adata,
+    color=["majority_voting", "conf_score"],
+    frameon=False,
+    sort_order=False,
+    wspace=0.3,
+)
+sc.pl.dendrogram(predictions_low_adata, groupby="majority_voting")
+sc.pl.dendrogram(predictions_high_adata, groupby="majority_voting")
+
+labelsLow = predictions_low_adata.obs[["majority_voting", "conf_score"]]
+labelsLow.columns = [f'{colName}_low' for colName in labelsLow.columns]
+
+labelsHigh = predictions_high_adata.obs[["majority_voting", "conf_score"]]
+labelsHigh.columns = [f'{colName}_high' for colName in labelsHigh.columns]
+
+adata.obs = adata.obs.join(labelsLow)
+adata.obs = adata.obs.join(labelsHigh)
+
 adata.write_h5ad('../data/processed/BRI-2937.h5ad')
 # %%
